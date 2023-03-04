54ee9b4b62ca02853cf53c071da55026663815fc
laogao <laogao@DESKTOP-SJRU4BH>
2023-03-04 16:10:43 +0800

    一个示例程序

--- InsightQuote/InsightQuote.cpp
+++ InsightQuote/InsightQuote.cpp
@@ -2,6 +2,7 @@
 //
 #include <vector>
 #include <fmt/ranges.h>
+#include <sqlite3.h>
 #include "InsightQuote.h"
 
 
@@ -11,4 +12,8 @@ int main()
 {
 	std::vector<int> v = { 1, 2, 3 };
 	fmt::print("{}\n", v);
+	sqlite3* targ = nullptr;
+	sqlite3_open("data.sqlit", &targ);
+	fmt::print("{}\n", (int64_t)targ);
+	fmt::print("end\n");
 }
