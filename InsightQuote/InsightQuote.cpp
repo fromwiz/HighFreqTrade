1951b9020f75272f4333f60d4bc1b386ae9ecf4a
laogao <laogao@DESKTOP-SJRU4BH>
2023-03-04 11:41:50 +0800

    Add project files.

---
+++ InsightQuote/InsightQuote.cpp
@@ -0,0 +1,12 @@
+﻿// HighFreqTrade.cpp : Defines the entry point for the application.
+//
+
+#include "InsightQuote.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
